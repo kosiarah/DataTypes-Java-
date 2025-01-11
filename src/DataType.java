f82873313bd32da3d1d8bf8f9315334d8b1b8974
kosiarah <kosiarah@gmail.com>
2025-01-10 22:04:08 -0500

    Here is the test from class 1

---
+++ src/DataType.java
@@ -0,0 +1,28 @@
+public class DataType {
+    public static void main(String[] args) {
+// DataTypes Test
+
+    int number = 24;
+    String name = "Kosi";
+    long population = 8000000L;
+    short year = 2024;
+    byte age = 20;
+    boolean married = false;
+    char gender = 'M';
+    float height = 5.0f;
+    double pi = 3.14;
+
+
+    System.out.println("The number is: " + number);
+    System.out.println("My name is: " + name);
+    System.out.println("The population is: " + population);
+    System.out.println("The year is: " + year);
+    System.out.println("I am " + age + " years old.");
+    System.out.println("I am married.: " + married);
+    System.out.println("Please input your gender: " + gender);
+    System.out.println("Please input your height: " + height);
+    System.out.println("What is thr value of pi?: " + pi);
+    }
+}
+
+// Sir i did not understand what you meant by "Modify the values of the variables and print the updated version"
